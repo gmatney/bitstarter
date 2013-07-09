e08edbf2e07ed9a7693561bc87110848d3c5f043
matney <garnett3m@gmail.com>
2013-07-09 02:24:42 +0000

    Getting index.html to be displayed

--- web.js
+++ web.js
@@ -1,12 +1,25 @@
 var express = require('express');
 
+
+var buf = fs.readFileSync("index.html");
+var index = buf.toString('utf-8');
+
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+var financeurl = function(symbols, columns) {
+    return util.format(
+	'http://finance.yahoo.com/d/quotes.csv?s=%s&f=%s',
+	symbols.join('+'),
+	columns);
+};
