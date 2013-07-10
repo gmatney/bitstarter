6a34b3ad89ea4e179dffbb2b6b9eb28d1e1f8f37
matney <garnett3m@gmail.com>
2013-07-10 14:06:51 +0000

    Adding grader files and associated tests

--- grader.js
+++ grader.js
@@ -21,54 +21,91 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var util = require('util');
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
-    var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
-    return instr;
+	var instr = infile.toString();
+	if(!fs.existsSync(instr)) {
+		console.log("%s does not exist. Exiting.", instr);
+		process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	}
+	return instr;
+};
+
+var buildFnCheerioHtmlUrlProcessor = function(instr,checksfile) {
+		var cheerioHtmlUrlProcessor = function(result, response) {
+				if (result instanceof Error) {
+					if(response === null){
+						console.error('No response received from "'+instr+'"');
+						process.exit(1);
+					}
+					else{
+						console.error('Error: ' + util.format(response.message));
+						console.error('Not able to download site "'+instr+'"');
+						process.exit(1);
+					}
+				} else {
+					$ = cheerio.load(result);
+					var checks = loadChecks(checksfile).sort();
+					var out = {};
+					for(var ii in checks) {
+						var present = $(checks[ii]).length > 0;
+						out[checks[ii]] = present;
+					}
+					var outJson = JSON.stringify(out, null, 4);
+					console.log(outJson);
+					
+				}
+		};
+		return cheerioHtmlUrlProcessor;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+	return cheerio.load(fs.readFileSync(htmlfile));
 };
 
 var loadChecks = function(checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
+	return JSON.parse(fs.readFileSync(checksfile));
 };
 
+
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
+	$ = cheerioHtmlFile(htmlfile);
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	for(var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	}
+	return out;
 };
 
 var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
-    return fn.bind({});
+	// Workaround for commander.js issue.
+	// http://stackoverflow.com/a/6772648
+	return fn.bind({});
 };
 
 if(require.main == module) {
-    program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	program
+		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <html_file_url>', 'url to index.html')
+		.parse(process.argv);
+	if(program.url != null){
+		rest.get(program.url).on('complete', buildFnCheerioHtmlUrlProcessor(program.url,program.checks));
+	}
+	else{
+		var checkJson = checkHtmlFile(program.file, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	}
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+	exports.checkHtmlFile = checkHtmlFile;
 }
---
+++ test_runner.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./grader.js -c test/checks-test-1.json -f test/test1.html 
+./grader.js -c test/checks-test-1.json -f test/test2.html 
+./grader.js -c test/checks-test-2.json -f test/test1.html 
+./grader.js -c test/checks-test-2.json -f test/test2.html 
+
