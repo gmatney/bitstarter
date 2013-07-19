ba8de7f1139d5e0124e626423dac9b03a10f8220
matney <garnett3m@gmail.com>
2013-07-19 02:29:53 +0000

    Adding style!  This is the twitter bootstrapping.  Most all changes are for the index.html.  Port got changed in web.js And there were left over changes for hw3 in the test_runner.
    Please enter the commit message for your changes. Lines starting

--- test_runner.sh
+++ test_runner.sh
@@ -2,5 +2,5 @@
 ./grader.js -c test/checks-test-1.json -f test/test1.html 
 ./grader.js -c test/checks-test-1.json -f test/test2.html 
 ./grader.js -c test/checks-test-2.json -f test/test1.html 
-./grader.js -c test/checks-test-2.json -f test/test2.html 
-
+./grader.js -c test/checks-test-2.json -f test/test2.html
+./grader.js --checks checks.json --url  http://hidden-everglades-8866.herokuapp.com/ 
